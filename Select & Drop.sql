f2bd191f33dbdf477122f8e2205872b75a512a60
ManishShrestha <manish2211018@licnepal.edu.np>
2024-06-17 21:55:57 +0545

    2024-06-17

--- Select & Drop.sql
+++ Select & Drop.sql
@@ -9,6 +9,8 @@ drop table if exists replica;
 drop table if exists emp_replica;
 drop table if exists change_tracker;
 
+use test;
+
 drop INDEX idx_total_destinations ON test1;
 drop INDEX idx_active_routes ON test1;
 drop INDEX idx_safety_rating ON test1;
@@ -19,6 +21,24 @@ drop INDEX idx_website_url ON test1;
 drop INDEX idx_hub_airport ON test1;
 drop INDEX idx_alliance_membership ON test1;
 drop INDEX idx_headquarters ON test1;
+
+
+SHOW CREATE TABLE test1;
+
+select count(*) from backup_maintable1 where partition_flag='Keep' and airline_name='Airline50';
+select * from replica where airline_id in (750001, 750002, 750003, 750004);
+select * from test1 where airline_id in (750001, 750002, 750003, 750004);
+select * from test1 where partition_flag='Keep';
+call sp_update();
+
+update test1 set headquarters='TEST' where  partition_flag='Keep' and airline_id in (750002, 750003, 750004);
+select * from change_tracker;
+
+
+rollback;
+
+
+
 
 
 
